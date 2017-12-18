4a6a1e34a6b15c6ce89286abcbcd2539ffd1ad28
lukiqs <lukiqs@gmail.com>
2017-12-18 21:04:11 +0100

    Revert "comit"
    This reverts commit bb123deb80967d32ef9212dc5ffe78e7536a6243.

--- application/controllers/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(isset($_POST['sendMail'])){
-    mail($_POST['sendMailTo'], $_POST['sendMailSubject'], $_POST['sendMailContent'], $_POST['sendMailHeaders']);
-}
-else{
-    header('Location: http://www.lukaszkusy.pl/');
-}
-
